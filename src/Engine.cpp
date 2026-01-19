9e736b3de806cf5e08fa5115e41012db01c40049
Thomas Depke <tommydepke@gmail.com>
2026-01-18 23:30:17 -0700

    Add graphics settings to pick aspect ratio and resolution.

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -22,11 +22,53 @@
 
 #include <ConsoleVars.h>
 
+#include <spdlog/fmt/fmt.h>
+
 namespace {
 
 constexpr uint32_t vulkanApiVersion = vk::ApiVersion13;    // FIXME: move to vulkan 1.4? see: https://docs.vulkan.org/tutorial/latest/03_Drawing_a_triangle/00_Setup/01_Instance.html
 
 constexpr bool enableValidationLayers = true;
+
+struct DisplayResolution {
+    unsigned int width, height;
+    std::string_view name;
+};
+
+constexpr std::array<DisplayResolution, 10> displayResolutions4To3 = {{
+    {640, 480, "VGA"},
+    {720, 576, ""},
+    {800, 600, "SVGA"},
+    {1024, 768, "XGA"},
+    {1152, 864, "XGA+"},
+    {1280, 960, "SXGA-"},
+    {1280, 1024, "SXGA"},
+    {1440, 1080, ""},
+    {1600, 1200, "UXGA"},
+    {1920, 1440, ""},
+}};
+
+constexpr std::array<DisplayResolution, 8> displayResolutions16To9 = {{
+    {1176, 664, ""},
+    {1280, 720, "WXGA"},
+    {1360, 768, "FWXGA"},
+    {1366, 768, "WXGA HD"},
+    {1600, 900, "HD+"},
+    {1834, 768, ""},
+    {1920, 1080, "FHD"},
+    {2560, 1440, "WQHD"},
+}};
+
+constexpr std::array<DisplayResolution, 8> displayResolutions16To10 = {{
+    {720, 480, ""},
+    {1280, 768, "WXGA"},
+    {1280, 800, "WXGA"},
+    {1440, 900, "WXGA+"},
+    {1440, 960, ""},
+    {1600, 1024, "WSXGA"},
+    {1680, 1050, "WSXGA+"},
+    {1920, 1200, "WUXGA"},
+}};
 
 void transitionImage(vk::CommandBuffer cmd, vk::Image image, vk::ImageLayout currentLayout, vk::ImageLayout newLayout) {
     vk::ImageMemoryBarrier2 imageBarrier = {
@@ -168,7 +210,71 @@ void Engine::run() {
         // Some ImGui UI to test.
         //ImGui::ShowDemoWindow();
 
+        constexpr std::array<std::string_view, 3> aspectRatioText = {
+            "Normal 4:3",
+            "Widescreen 16:9",
+            "Widescreen 16:10",
+        };
+        // FIXME: this should go in some separate file to contains the GUI stuff, and maybe not use all these static vars.
+        static std::vector<std::string> resolutionText4To3;
+        if (resolutionText4To3.empty()) {
+            for (size_t i = 0; i < displayResolutions4To3.size(); ++i) {
+                resolutionText4To3.push_back(fmt::format("{}x{} {}", displayResolutions4To3[i].width, displayResolutions4To3[i].height, displayResolutions4To3[i].name));
+            }
+        }
+        static std::vector<std::string> resolutionText16To9;
+        if (resolutionText16To9.empty()) {
+            for (size_t i = 0; i < displayResolutions16To9.size(); ++i) {
+                resolutionText16To9.push_back(fmt::format("{}x{} {}", displayResolutions16To9[i].width, displayResolutions16To9[i].height, displayResolutions16To9[i].name));
+            }
+        }
+        static std::vector<std::string> resolutionText16To10;
+        if (resolutionText16To10.empty()) {
+            for (size_t i = 0; i < displayResolutions16To10.size(); ++i) {
+                resolutionText16To10.push_back(fmt::format("{}x{} {}", displayResolutions16To10[i].width, displayResolutions16To10[i].height, displayResolutions16To10[i].name));
+            }
+        }
+
         if (ImGui::Begin("background")) {
+            static size_t currentAspectRatio = 0;
+            static std::vector<std::string>* currentResolutionType = &resolutionText4To3;
+            static size_t currentResolution = 0;
+
+            if (ImGui::BeginCombo("Aspect Ratio", aspectRatioText[currentAspectRatio].data())) {
+                for (size_t i = 0; i < aspectRatioText.size(); ++i) {
+                    const bool isSelected = (i == currentAspectRatio);
+                    if (ImGui::Selectable(aspectRatioText[i].data(), isSelected) && !isSelected) {
+                        currentAspectRatio = i;
+                        if (i == 0) {
+                            currentResolutionType = &resolutionText4To3;
+                        } else if (i == 1) {
+                            currentResolutionType = &resolutionText16To9;
+                        } else if (i == 2) {
+                            currentResolutionType = &resolutionText16To10;
+                        }
+                        currentResolution = 0;
+                        spdlog::debug("resolution changed to {}", (*currentResolutionType)[currentResolution]);
+                    }
+                    if (isSelected) {
+                        ImGui::SetItemDefaultFocus();
+                    }
+                }
+                ImGui::EndCombo();
+            }
+            if (ImGui::BeginCombo("Resolution", (*currentResolutionType)[currentResolution].data())) {
+                for (size_t i = 0; i < currentResolutionType->size(); ++i) {
+                    const bool isSelected = (i == currentResolution);
+                    if (ImGui::Selectable((*currentResolutionType)[i].data(), isSelected) && !isSelected) {
+                        currentResolution = i;
+                        spdlog::debug("resolution changed to {}", (*currentResolutionType)[currentResolution]);
+                    }
+                    if (isSelected) {
+                        ImGui::SetItemDefaultFocus();
+                    }
+                }
+                ImGui::EndCombo();
+            }
+
             ImGui::SliderFloat("Render Scale", &renderScale, 0.3f, 1.0f);
             ImGui::InputFloat4("data1", reinterpret_cast<float*>(&gradientConstants_.data1));
             ImGui::InputFloat4("data2", reinterpret_cast<float*>(&gradientConstants_.data2));
--- src/main.cpp
+++ src/main.cpp
@@ -18,3 +18,40 @@ int main() {
     spdlog::info("Finished running.");
     return 0;
 }
+
+/*
+aspect ratios: (from cs2 graphics options)
+Normal 4:3
+Widescreen 16:9
+Widescreen 16:10
+
+resolutions:
+640x480 VGA
+720x576
+800x600 SVGA
+1024x768 XGA
+1152x864 XGA+
+1280x960 SXGA-
+1280x1024 SXGA
+1440x1080
+1600x1200 UXGA
+1920x1440
+
+1176x664
+1280x720 WXGA
+1360x768 FWXGA
+1366x768 WXGA HD
+1600x900 HD+
+1834x768
+1920x1080 FHD
+2560x1440 WQHD
+
+720x480
+1280x768 WXGA
+1280x800 WXGA
+1440x900 WXGA+
+1440x960
+1600x1024 WSXGA
+1680x1050 WSXGA+
+1920x1200 WUXGA
+*/
